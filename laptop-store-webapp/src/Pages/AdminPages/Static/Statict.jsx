f7a6b91c43b10b66052750be2ae141dae6b0dc09
lthoa462 <lthoa462@gmail.com>
2021-12-19 21:37:55 +0700

    update bao cao

--- laptop-store-webapp/src/Pages/AdminPages/Static/Statict.jsx
+++ laptop-store-webapp/src/Pages/AdminPages/Static/Statict.jsx
@@ -10,33 +10,36 @@ import moment from 'moment';
 
 
 export default function Statict ({ }){
-    const [select, setSelect] = useState("");
+    const [select, setSelect] = useState("month");
     const [dateRange, setDateRange] = useState([null, null]);
     const [year, setYear] = useState(null);
+    const [day, setDay] = useState(null);
+    const [month, setMonth] = useState(null);
     const [startDate, endDate] = dateRange;
     const [datares, setDatares] = useState([]);
     const [flag, setFlag] = useState(false);
     const [label, setLabel] = useState([]);
     const [datas, setData] = useState([]);
+    const [isnull, setIsnull] = useState(false);
     const showSelect = () =>{
         switch (select) {
             case "day":
                 return(
                     <div className="statict-form-value">
                         <p className="static-form-label" >Nhập ngày cần tìm: </p>
-                        <input type="date" className="static-input"></input>
+                        <input type="date" className="static-input" onChange={(e)=>handelClickStatict(e)}></input>
                     </div>);
             case "month":
                 return(
                     <div className="statict-form-value">
                         <p className="static-form-label" >Nhập tháng cần tìm: </p>
-                        <input type="month" className="static-input"></input>
+                        <input type="month" className="static-input" onChange={(e)=>handelClickStatict(e)}></input>
                     </div>)
             case "year":
                 return(
                     <div className="statict-form-value">
                         <p className="static-form-label">Nhập năm cần tìm: </p>
-                        <input type="number" min="1900" max="2099" step="1" defaultValue="2021" className="static-input"/>
+                        <input type="number" min="1900" max="2099" step="1" defaultValue="2021" className="static-input" onChange={(e)=>handelClickStatict(e)}/>
                     </div>);
             case "fromto":
                 return (
@@ -51,7 +54,6 @@ export default function Statict ({ }){
                         }}
                     />
                     </div>
-                    
                 )
             default:
                 break;
@@ -60,50 +62,114 @@ export default function Statict ({ }){
         
     }
     
-    
       const handleClick = () =>{
-          axios.get(`https://localhost:44343/data/statistics/bill/month=12/year=2021`)
+        switch (select) {
+            case "day":
+                if (datares.length!==0)
+                {                
+                    datares.forEach((data)=>{
+                        label.push(data.id);
+                        datas.push(data.tongtien);
+                })}
+                else setIsnull(true);  
+                break;
+            case "month":
+                if (datares.length!==0)
+                {
+                    datares.forEach((datare)=>{
+                        const temp = (moment(datare.ngaydat).toDate().getDate());
+                        datas.splice(temp-1,1,datare.tongtien);
+                    });
+                } else setIsnull(true);
+                break;
+            case "year": 
+                if (datares.length!==0)
+                {
+                   datares.forEach((datare)=>{
+                    const temp = (moment(datare.ngaydat).toDate().getMonth());
+                       datas.splice(temp-1,1,datas[temp-1]+datare.tongtien);
+                   })
+                } else setIsnull(true);
+                break;
+        }
+      }
+      const handelClickStatict = (e) =>{
+          let value = e.target.value;
+        switch (select) {
+            case "day":
+                let d = (moment(value).toDate().getMonth()+1).toString();
+                d = d+"-"+moment(value).toDate().getDate().toString() + "-" + moment(value).toDate().getFullYear().toString();
+                console.log(d);
+                axios.get(`https://localhost:44343/data/statistics/bill/date=${d}`)
                 .then((res)=>{
                     setDatares(res.data);
-
+                    console.log(datares);
+                })
+                .catch ((err)=>console.log(err));	
+                console.log(label);		
+                break;
+            case "month":
+                axios.get(`https://localhost:44343/data/statistics/bill/month=${moment(value).toDate().getMonth()+1}/year=${moment(value).toDate().getFullYear().toString()}`)
+                .then((res)=>{
+                    setDatares(res.data);
                     console.log(datares);
                 })
                 .catch ((err)=>console.log(err));
-        const dim=moment("2021-12", "YYYY-MM").daysInMonth();
-        for (let i=1;i<=dim;i++)
-        {
-            label.push(i.toString());
-            datas.push(0);
-        }
-        datares.forEach((datare)=>{
-            const temp = (moment(datare.ngaydat).toDate().getDate());
-            datas.splice(temp-1,1,datare.tongtien);
-            console.log(typeof datare.tongtien);
-        });
-        setFlag(true);
+                let z =  moment(value).toDate().getFullYear().toString()+ "-" + moment(value).toDate().getMonth()+1;
+                console.log(moment(z,"yyyy mm").daysInMonth());	
+                if (label.length!==(moment(z,"yyyy mm").daysInMonth()))
+                    for (let i=1; i<=moment(z,"yyyy mm").daysInMonth();i++)
+                    {
+                        label.push(i);
+                        datas.push(0);
+                    }
+                    datares.forEach((datare)=>{
+                        const temp = (moment(datare.ngaydat).toDate().getDate());
+                        datas.splice(temp-1,1,datare.tongtien);
+                    });
+                break;
+            case "year":
+                axios.get(`https://localhost:44343/data/statistics/bill/year/${value}`)
+                .then((res)=>{
+                    setDatares(res.data);
+                    console.log(datares);
+                })
+                .catch ((err)=>console.log(err));
+                let temp = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+                setLabel(temp);
+                console.log(label);
+                break;
       }
-      const data = {
-        labels: label,
-        datasets:[{
-            label: "Doanh thu",
-            data: datas,
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)"
-          }] 
-      };
-      const showne = () => {
-          if (flag===true)
-          console.log("hehe");
       }
 
+      const showchart = () =>{
+          console.log(datas);
+          console.log(label);
+          if (label && datas)
+          {
+            const data = {
+                labels: label,
+                datasets:[{
+                    label: "Doanh thu",
+                    data: datas,
+                    fill: true,
+                    backgroundColor: "rgba(75,192,192,0.2)",
+                    borderColor: "rgba(75,192,192,1)"
+                  }] 
+              };
+              console.log(data);
+            return (
+                <Line data={data} className="statict-chart"/>
+            );
+          } else return (<p>Vui lòng chọn thời gian</p>);
+      }
     return (
         <div className="statict-main">
             <div className="statict-layout">
                 <div className="statict-header"> <h2>Thống kê doanh thu</h2></div>
                 <div className="statict-main-page">
                     <div className="statict-top-button">
-                        <select name="statict type" className="statict-select" onChange={(e)=>setSelect(e.target.value)}>
+                        <select name="statict type" className="statict-select" defaultValue="month" onChange={(e)=>setSelect(e.target.value)}>
                             <option value="day" >Ngày</option>
                             <option value="month">Tháng</option>
                             <option value="year">Năm</option>
@@ -113,7 +179,8 @@ export default function Statict ({ }){
                         <button onClick={()=>handleClick()}>Chọn</button>
                     </div>
                     <div className="statict-chart-panel">
-                        <Line data={data} className="statict-chart"/>
+                        <div className=""></div>
+                        {showchart()}
                     </div>
                 </div>
             </div>
